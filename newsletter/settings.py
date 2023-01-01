cfdbc68ab117a30cd36fd2be821e6f7bede84e05
rk_man <naveenbadi2003@gmail.com>
2023-01-01 10:49:22 +0530

    few changes to env file

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -17,7 +17,7 @@ import os
 #
 env = environ.Env()
 #
-# environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
