05aa0613893f6eb1cc3acbd467d265d385088855
rk_man <naveenbadi2003@gmail.com>
2023-01-26 17:35:50 +0530

    Changes staticfiles dir

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
